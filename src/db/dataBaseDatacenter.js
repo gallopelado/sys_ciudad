f2c0612bf0619ed56b6e904f700eb6bd3085a4c3
Fernando Asilvera <fernandoasilvera@gmail.com>
2022-10-28 18:47:56 -0300

    fixes

--- src/db/dataBaseDatacenter.js
+++ src/db/dataBaseDatacenter.js
@@ -2,7 +2,7 @@ const pgp = require('pg-promise')(/*options*/);
 const CnDc = {
     host: '',
     port: 5432,
-    database: 'datacenter_new',
+    database: '',
     user: '',
     password: '',
     max: 30 // use up to 30 connections
